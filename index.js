07024569a94e10fe67c9383fbb674df9da27ae9c
JJ-blossom <73256107@protonmail.com>
2026-01-25 20:33:48 +0000

    Add game over function to handle game over message and game reset.

--- index.js
+++ index.js
@@ -3,24 +3,31 @@ const startContainer = document.querySelector(".start-container");
 const gameWindow = document.querySelector(".game-window");
 const endZone = document.querySelector(".end-zone");
 const playerCharacter = document.querySelector("#playerCharacter");
-const vehicleList = document.querySelector(".vehicle-list");
 const carListItems = document.querySelectorAll("li");
 const levelDisplay = document.querySelector(".level-display");
-let gameCount = 1;
+const instructions = document.querySelector(".instructions");
 
+let gameCount = 1;
+let animationID;
 let playerSpeed = 5;
 let carSpeed = 5;
 let carList = [];
-
+//eventlistener function
+const keyDown = (event) => {
+  if (event.key === 37 || event.code === "ArrowDown") {
+    currentPosition = playerCharacter.offsetTop;
+    movePlayer(currentPosition);
+  }
+};
 //move player down the page
 const movePlayer = (yPosition) => {
   if (yPosition < endZone.getBoundingClientRect().top) {
-    // console.log(yPosition, endZone.getBoundingClientRect().top);
     playerCharacter.style.top = `${yPosition + playerSpeed}px`;
   } else if (yPosition >= endZone.getBoundingClientRect().top - 10) {
     // player in endzone
     levelUp();
   }
+  // collision detection between player and all cars
   carListItems.forEach((vehicle) => {
     let player = playerCharacter.getBoundingClientRect();
     let car = vehicle.getBoundingClientRect();
@@ -31,7 +38,7 @@ const movePlayer = (yPosition) => {
       player.y <= car.y + car.height &&
       player.y + player.height >= car.y
     ) {
-      console.log("collision detected");
+      gameOver(gameCount);
     }
   });
 };
@@ -48,28 +55,38 @@ const levelUp = () => {
   playerSpeed *= 1.2;
   carSpeed *= 1.2;
 };
-
+// gameOver
+const gameOver = (gameCount) => {
+  // cancel animation and event listeners, reset player, display gamecount and message
+  cancelAnimationFrame(animationID);
+  window.removeEventListener("keydown", keyDown);
+  resetPlayer();
+  gameCount === 1
+    ? (instructions.innerHTML =
+        "Wow, Not even once? Better luck next time. Click on Let's go to try again")
+    : (instructions.innerHTML = `Welldone! you helped Pengoo across ${gameCount} times.  Click on Let's go to play again`);
+  startContainer.classList.toggle("hide-this");
+  gameWindow.classList.toggle("hide-this");
+  playerCharacter.classList.toggle("hide-this");
+};
 //create vehicles
-const createCar = () => {
+const createCar = (top) => {
+  const newElement = document.createElement("li");
   const car = document.createElement("img");
-  car.src = `./assets/${Math.ceil(Math.random() * 2)}.png`;
+  car.src = `./assets/${Math.ceil(Math.random() * 10)}.png`;
   car.alt = "picture of a car";
   car.style.position = "absolute";
   car.style.top = gameWindow.getBoundingClientRect().top;
-  car.style.left = startLeftPos;
-  car.offsetLeft = "40vw";
-  car.offsetTop = "100px";
-  carList.push(car);
+  car.offsetLeft = "0vw";
+  car.offsetTop = `${top}`;
+  //append child etc
   return;
 };
 // continue game
 const continueGame = (gamecount) => {
   // move car
   const viewPortWidth = window.innerWidth;
-  const endZonePosition = gameWindow.getBoundingClientRect().top - 25;
-
   let startLeftPos = Math.floor(Math.random() * viewPortWidth);
-
   const moveEachVehicle = (timestamp) => {
     carListItems.forEach((car) => {
       if (startLeftPos >= viewPortWidth) {
@@ -79,18 +96,12 @@ const continueGame = (gamecount) => {
         car.style.marginLeft = startLeftPos + "px";
       }
       startLeftPos += 1;
-
-      requestAnimationFrame(moveEachVehicle);
     });
+    requestAnimationFrame(moveEachVehicle);
   };
-  requestAnimationFrame(moveEachVehicle);
-
-  window.addEventListener("keydown", (event) => {
-    if (event.key === 37 || event.code === "ArrowDown") {
-      currentPosition = playerCharacter.offsetTop;
-      movePlayer(currentPosition);
-    }
-  });
+  animationID = requestAnimationFrame(moveEachVehicle);
+  //event listener
+  window.addEventListener("keydown", keyDown);
 };
 
 //start the game
@@ -98,7 +109,6 @@ const startGame = () => {
   startContainer.classList.toggle("hide-this");
   gameWindow.classList.toggle("hide-this");
   playerCharacter.classList.toggle("hide-this");
-  // console.log(window.innerHeight, gameWindow.getBoundingClientRect().bottom);
   continueGame();
 };
 
